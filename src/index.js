12df5eb8cdb609f062f7c550c7645a1a761ccccd
Renato Bonfim Herculano <re_herculano@hotmail.com>
2022-09-16 20:43:21 -0300

    primeira parte da aula

--- src/index.js
+++ src/index.js
@@ -1,59 +1,95 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import ReactDOM from 'react-dom';
-import React from 'react';
+import "bootstrap/dist/css/bootstrap.css";
+import ReactDOM from "react-dom";
+import React from "react";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: null,
+      longitude: null,
+      estacao: null,
+      data: null,
+      icone: null,
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            latitude: null,
-            longitude: null,
-            estacao: null,
-            data: null,
-            icone: null
-        }
+  obterEstacao(data, latitude) {
+    const ano = data.getFullYear();
+    const d1 = new Date(ano, 5, 21);
+    const d2 = new Date(ano, 8, 24);
+    const d3 = new Date(ano, 11, 22);
+    const d4 = new Date(ano, 3, 21);
+
+    const sul = latitude < 0;
+
+    if (data >= d1 && data < d2) {
+      return sul ? "inverno" : "verão";
     }
 
-    obterEstacao(data, latitude) {
-        const ano = data.getFullYear();
-        const d1 = new Date(ano, 5, 21);
-        const d2 = new Date(ano, 8, 24);
-        const d3 = new Date(ano, 11, 22);
-        const d4 = new Date(ano, 3, 21);
-
-        const sul = latitude < 0;
-
-        if (data >= d1 && data < d2) {
-            return sul ? 'inverno' : 'verão';
-        }
-
-        if (data >= d2 && data < d3) {
-            return sul ? 'primavera' : 'outono';
-        }
-
-        if (data >= d3 && data < d4) {
-            return sul ? 'verão' : 'inverno';
-        }
-
-        return sul ? 'outono' : 'primavera';
+    if (data >= d2 && data < d3) {
+      return sul ? "primavera" : "outono";
     }
 
-    icons = {
-        'verão': 'fa-sun',
-        'outuno': 'fa-tree',
-        'inverno': 'fa-snowman',
-        'primavera': 'fa-seeding'
+    if (data >= d3 && data < d4) {
+      return sul ? "verão" : "inverno";
     }
 
-    render() {
-        return (
-            <div>Meu App</div>
-        );
-    }
+    return sul ? "outono" : "primavera";
+  }
+
+  icons = {
+    verão: "fa-sun",
+    outuno: "fa-tree",
+    inverno: "fa-snowman",
+    primavera: "fa-seeding",
+  };
+
+  obterLocalizacao() {
+    window.navigator.geolocation.getCurrentPosition((position) => {
+      let data = new Date();
+      let estacao = this.obterEstacao(data, position.coords.latitude);
+      let icone = this.icons[estacao];
+      this.setState({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        data: data.toLocaleString(),
+        estacao: estacao,
+        icone: icone,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="container mt-2">
+        <div className="row justify-content-center">
+          <div className="col-md-8">
+            <div className="card">
+              <div className="card-body">
+                <div
+                  className="d-flex align-items-center border rounded mb-2"
+                  style={{ height: "6rem" }}
+                >
+                    <i className={`fas fa-5x ${this.state.icone}`}></i>
+                    <p className="w-75 ms-3 text-center fs-1">{`${this.state.estacao}`}</p>
+                </div>
+                <div>
+                    <p className="text-center">
+                        {
+                            this.state.latitude 
+                            ? `Coordenadas: ${this.state.latitude}, ${this.state.lagitude}, Data: ${this.state.data}` 
+                            : `Clique no botão para saber a sua estação climática`
+                        }
+                    </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(
-    <App/>,
-    document.querySelector('#root')
-)
+ReactDOM.render(<App />, document.querySelector("#root"));
